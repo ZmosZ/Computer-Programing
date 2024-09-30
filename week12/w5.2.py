b1b605ccc796129faf159df9e0e1a5b5a728c4dc
ZmosZ <154952681+ZmosZ@users.noreply.github.com>
2024-09-30 15:35:59 +0700

    fix week12

--- week12/w5.2.py
+++ week12/w5.2.py
@@ -4,5 +4,8 @@ try:
     with open('b.csv','w',encoding='utf-8') as f:
         w = writer(f,lineterminator='\n')
         w.writerow([a])
+    f = open('b.txt','w',encoding='utf-8')
+    f.write(str(a))
+    f.close()
 except Exception as e:
     print(e)
